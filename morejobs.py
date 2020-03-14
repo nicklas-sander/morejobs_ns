77fe50d5851522f7289197e982d73040af174040
nicklas-sander <ebbesander@arcor.de>
2020-03-14 14:36:04 +0100

    Train Test Split added

--- morejobs.py
+++ morejobs.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import warnings
 
